58eb3584d3256e6ffd1830818f108c1776a59aaa
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-06-03 00:35:45 +0800

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import {setSearchField} from '../actions';
+
+const mapStateToProps = state =>{
+  return {
+    searchField: state.searchField
+  }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+  return{
+    onSearchChange:(event)=>dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends React.Component{
   constructor(){
     super();
     this.state={
-      robots: [],
-      searchfield:''
+      robots: []
     }
   }
 
@@ -20,21 +34,20 @@ class App extends React.Component{
 
   }
 
-  onSearchChange=(event)=>{
-    this.setState({searchfield:event.target.value})
-  }
 
   render(){
-    const filterRobot = this.state.robots.filter(robot=>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    const {robots} = this.state;
+    const {searchField, onSearchChange} = this.props
+    const filterRobot = robots.filter(robot=>{
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
     if(this.state.robots.length === 0){
       return <h1>Loading</h1>
     } else {
   return(
     <div>
-    <h1 className='f1 tc'>RobotFriends</h1>
-    <SearchBox searchChange={this.onSearchChange}/>
+    <h1 className='f1 tc'>RoboFriends</h1>
+    <SearchBox searchChange={onSearchChange}/>
     <Scroll>
     <CardList robots={filterRobot} />
     </Scroll>
@@ -45,4 +58,4 @@ class App extends React.Component{
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
 import App from './containers/App';
+import {createLogger} from 'redux-logger';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import {searchRobots} from './reducers';
+
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 
 ReactDOM.render(
+      <Provider store={store}>
       <App />
+      </Provider>
 , document.getElementById('root'));
 registerServiceWorker();
